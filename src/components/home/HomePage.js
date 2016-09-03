0d3e975be1b056dd79c8f04c0161091dc7010b1f
compumike08 <compumike08@users.noreply.github.com>
2016-09-03 18:14:54 -0400

    Invoke GithubAPI directly from HomePage component & output returned data to console

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import GithubAPI from '../../api/githubAPI';
 
 class HomePage extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      showResult: {}
+    };
+
+  }
+
+  componentDidMount(){
+    GithubAPI.testOctokat().then(testResult => {
+      this.setState({showResult: testResult});
+      console.log(testResult);
+    }).catch(error => {
+      throw(error);
+    });
+
+  }
+
     render() {
         return (
-          <h1>GitHub Status API GUI</h1>
+          <div>
+            <h1>GitHub Status API GUI</h1>
+            <h3>Open browser console to see JSON data returned from GitHub API</h3>
+          </div>
         );
     }
 }
