61b230f97eb16176a2071fb5086b9e11cddda660
compumike08 <compumike08@users.noreply.github.com>
2016-09-03 16:08:09 -0400

    Set up src/index.js, App.js, HomePage, routes.js, root reducer, & initialState.js

---
+++ src/components/home/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class HomePage extends React.Component {
+    render() {
+        return (
+          <h1>GitHub Status API GUI</h1>
+        );
+    }
+}
+
+export default HomePage;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-// Set up your application entry point here...
+/* eslint-disable import/default */
+
+import 'babel-polyfill';
+import React from 'react';
+import {render} from 'react-dom';
+import configureStore from './store/configureStore';
+import {Provider} from 'react-redux';
+import {Router, browserHistory} from 'react-router';
+import routes from './routes';
+require('./favicon.ico'); // Tell webpack to load favicon.ico
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/toastr/build/toastr.min.css';
+import '../node_modules/font-awesome/css/font-awesome.min.css';
+import { syncHistoryWithStore } from 'react-router-redux';
+
+const store = configureStore();
+
+// Create an enhanced history that syncs navigation events with the store
+const history = syncHistoryWithStore(browserHistory, store);
+
+render(
+    <Provider store={store}>
+        <Router history={history} routes={routes}/>
+    </Provider>,
+    document.getElementById('app')
+);
