c2c9a751e0a2a63beb4609325a25e5dbc19738d1
compumike08 <compumike08@users.noreply.github.com>
2016-09-11 12:59:12 -0400

    Modify HomePage to display GitHub repo updatedAt date from API when button clicked

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -6,29 +6,44 @@ class HomePage extends React.Component {
     super(props, context);
 
     this.state = {
-      showResult: {}
+      showResult: "l"
     };
+
+    this.invokeGitHubAPI = this.invokeGitHubAPI.bind(this);
 
   }
 
-  componentDidMount(){
+  invokeGitHubAPI(){
     GithubAPI.testOctokat().then(testResult => {
-      this.setState({showResult: testResult});
+      let parsedTestResult = testResult.updatedAt.toDateString();
       console.log(testResult);
+      console.log(parsedTestResult);
+      this.setState({showResult: parsedTestResult});
     }).catch(error => {
       throw(error);
     });
-
   }
 
-    render() {
-        return (
-          <div>
-            <h1>GitHub Status API GUI</h1>
-            <h3>Open browser console to see JSON data returned from GitHub API</h3>
+  render() {
+      return (
+        <div>
+          <h1>GitHub Status API GUI</h1>
+          <h3>Open browser console to see JSON data returned from GitHub API</h3>
+
+          <div className="row">
+            <div className="col-sm-3">
+              <button type="button" className="btn btn-primary" onClick={this.invokeGitHubAPI}>Test GitHub API Call</button>
+            </div>
           </div>
-        );
-    }
+
+          <div className="row">
+            <div className="col-sm-6">
+              <span>{this.state.showResult}</span>
+            </div>
+          </div>
+        </div>
+      );
+  }
 }
 
 export default HomePage;
