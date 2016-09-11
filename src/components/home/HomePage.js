634c3a1239bd736b9bc0de9e92ecef9068511c72
compumike08 <compumike08@users.noreply.github.com>
2016-09-11 13:20:52 -0400

    Improved UI display of returned pushedAt time

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -6,7 +6,7 @@ class HomePage extends React.Component {
     super(props, context);
 
     this.state = {
-      showResult: "NO DATA - Click button above to fetch data."
+      showResult: "NO DATA - Click button below to fetch data."
     };
 
     this.invokeGitHubAPI = this.invokeGitHubAPI.bind(this);
@@ -30,15 +30,18 @@ class HomePage extends React.Component {
           <h1>GitHub Status API GUI</h1>
           <h3>Open browser console to see JSON data returned from GitHub API</h3>
 
-          <div className="row">
-            <div className="col-sm-3">
-              <button type="button" className="btn btn-primary" onClick={this.invokeGitHubAPI}>Test GitHub API Call</button>
+          <div className="form-horizontal">
+            <div className="form-group">
+              <label htmlFor="pushedDateTime" className="col-sm-2 control-label">Date/Time of Last Push to Repo:</label>
+              <div className="col-sm-6">
+                <input id="pushedDateTime" className="form-control" readOnly value={this.state.showResult}/>
+              </div>
             </div>
           </div>
 
           <div className="row">
-            <div className="col-sm-6">
-              <span>{this.state.showResult}</span>
+            <div className="col-sm-3">
+              <button type="button" className="btn btn-primary" onClick={this.invokeGitHubAPI}>Test GitHub API Call</button>
             </div>
           </div>
         </div>
