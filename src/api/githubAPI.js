2197aa8a6246348070b9c26693ec08fc44f02031
compumike08 <compumike08@users.noreply.github.com>
2016-09-18 02:11:45 -0400

    Modify GithubApi to be able to set an OAuth token when instantiating

--- src/api/githubAPI.js
+++ src/api/githubAPI.js
@@ -2,6 +2,20 @@ let Octokat = require('octokat');
 let octo = new Octokat({});
 
 class GithubApi {
+  constructor(){
+    this.octo = new Octokat({});
+  }
+
+  static newOctokatNoToken(){
+    this.octo = new Octokat({});
+  }
+
+  static newOctokatWithToken(token){
+    this.octo = new Octokat({
+      token: token
+    });
+  }
+
   static testOctokat(){
     return new Promise((resolve) => {
       resolve(octo.repos('compumike08', 'GitHub_Status_API_GUI').fetch());
