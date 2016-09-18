e4293beb1e97fb9b4dd294925abc3744c15a5555
compumike08 <compumike08@users.noreply.github.com>
2016-09-18 00:03:45 -0400

    Add constants file

---
+++ src/utils/constants.js
@@ -0,0 +1,4 @@
+export const CLIENT_ID = "ca49494ef26794d2427f";
+export const GATEKEEPER_AUTH_URL = "https://gatekeeper-server.herokuapp.com/authenticate/";
+export const OAUTH_AUTHORIZE_URL = "https://github.com/login/oauth/authorize?client_id=ca49494ef26794d2427f";
+export const OAUTH_PROVIDER_NAME = "GitHub";
