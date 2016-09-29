90451a2f5b74f98c64652d172d9457437a275197
compumike08 <compumike08@users.noreply.github.com>
2016-09-29 03:09:52 -0400

    Add addTokenToOcto method to GithubApi class

--- src/api/githubAPI.js
+++ src/api/githubAPI.js
@@ -2,6 +2,12 @@ let Octokat = require('octokat');
 let octo = new Octokat({});
 
 class GithubApi {
+  static addTokenToOcto(token){
+    octo = new Octokat({
+      token: token
+    });
+  }
+
   static testOctokat(){
     return new Promise((resolve) => {
       resolve(octo.repos('compumike08', 'GitHub_Status_API_GUI').fetch());
