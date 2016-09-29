327ee6447e7438b7499ced8b55598885757d81b2
compumike08 <compumike08@users.noreply.github.com>
2016-09-29 03:54:00 -0400

    Add error handling to testOctokat method in GithubApi

--- src/api/githubAPI.js
+++ src/api/githubAPI.js
@@ -1,3 +1,5 @@
+import * as CONSTANTS from '../utils/constants';
+
 let Octokat = require('octokat');
 let octo = new Octokat({});
 
@@ -9,8 +11,19 @@ class GithubApi {
   }
 
   static testOctokat(){
-    return new Promise((resolve) => {
-      resolve(octo.repos('compumike08', 'GitHub_Status_API_GUI').fetch());
+    return new Promise((resolve, reject) => {
+      octo.repos('compumike08', 'GitHub_Status_API_GUI').fetch().then(result => {
+        resolve(result);
+      }).catch(error => {
+        let errObj = JSON.parse(error.message);
+
+        if(errObj.message == CONSTANTS.GITHUB_ERR_BAD_CREDENTIALS){
+          reject("ERROR: Bad credentials");
+        }else{
+          console.log(error);
+          reject("ERROR: An unknown error has occurred.");
+        }
+      });
     });
   }
 }
