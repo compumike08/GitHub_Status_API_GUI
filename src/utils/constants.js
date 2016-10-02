3382027c76a24527c3323af7c4695b4178a27c8e
compumike08 <compumike08@users.noreply.github.com>
2016-10-02 17:26:49 -0400

    Change 'GITHUB_REPO_NAME' constant to point to my node.js GitHub repo

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,6 +5,6 @@ export const OAUTH_PROVIDER_NAME = "GitHub";
 export const OAUTH_GH_REPO_STATUS_SCOPE = "repo:status";
 
 export const GITHUB_USER_NAME = "compumike08";
-export const GITHUB_REPO_NAME = "GitHub_Status_API_GUI";
+export const GITHUB_REPO_NAME = "node.js";
 
 export const GITHUB_ERR_BAD_CREDENTIALS = "Bad credentials";
