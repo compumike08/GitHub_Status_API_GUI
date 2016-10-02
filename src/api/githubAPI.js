7451531158824f3dc8d44b1e4224a774124bd39f
compumike08 <compumike08@users.noreply.github.com>
2016-10-02 19:04:58 -0400

    Remove test API function from GithubAPI & from HomePage UI

--- src/api/githubAPI.js
+++ src/api/githubAPI.js
@@ -1,6 +1,5 @@
 import {GITHUB_USER_NAME,
-  GITHUB_REPO_NAME,
-  GITHUB_ERR_BAD_CREDENTIALS} from '../utils/constants';
+  GITHUB_REPO_NAME} from '../utils/constants';
 import * as Axis from '../../node_modules/axis.js';
 
 let Octokat = require('octokat');
@@ -51,23 +50,6 @@ class GithubApi {
         reject("ERROR: Invalid commitRef parameter passed to GithubApi component.");
       }
 
-    });
-  }
-
-  static testOctokat(){
-    return new Promise((resolve, reject) => {
-      octo.repos(GITHUB_USER_NAME, GITHUB_REPO_NAME).fetch().then(result => {
-        resolve(result);
-      }).catch(error => {
-        let errMsg = getErrorResponseMsg(error);
-
-        if(errMsg == GITHUB_ERR_BAD_CREDENTIALS){
-          reject("ERROR: Bad credentials");
-        }else{
-          console.log(error);
-          reject("ERROR: An unknown error has occurred.");
-        }
-      });
     });
   }
 }
--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -7,24 +7,7 @@ class HomePage extends React.Component {
   constructor(props, context) {
     super(props, context);
 
-    this.state = {
-      showResult: "NO DATA - Click button below to fetch data."
-    };
-
-    this.invokeGitHubAPI = this.invokeGitHubAPI.bind(this);
     this.invokeGetStatusesForCommit = this.invokeGetStatusesForCommit.bind(this);
-  }
-
-  invokeGitHubAPI(){
-    GithubAPI.testOctokat().then(testResult => {
-      let parsedDateResult = testResult.pushedAt.toDateString();
-      let parsedTimeResult = testResult.pushedAt.toTimeString();
-      let parsedDateTimeResult = parsedDateResult + " --- " + parsedTimeResult;
-
-      this.setState({showResult: parsedDateTimeResult});
-    }).catch(error => {
-      throw(error);
-    });
   }
 
   invokeGetStatusesForCommit(){
@@ -41,15 +24,6 @@ class HomePage extends React.Component {
           <h1>GitHub Status API GUI</h1>
           <h3>Open browser console to see JSON data returned from GitHub API</h3>
 
-          <div className="form-horizontal">
-            <div className="form-group">
-              <label htmlFor="pushedDateTime" className="col-sm-2 control-label">Date/Time of Last Push to Repo:</label>
-              <div className="col-sm-6">
-                <input id="pushedDateTime" className="form-control" readOnly value={this.state.showResult}/>
-              </div>
-            </div>
-          </div>
-
           <div className="row">
             <div className="col-sm-3">
               <OAuthSignInButton/>
@@ -59,9 +33,6 @@ class HomePage extends React.Component {
           <div className="row">
             <div className="col-sm-6">
               <div className="btn-toolbar">
-                <div className="btn-group">
-                  <button type="button" className="btn btn-primary " onClick={this.invokeGitHubAPI}>Test GitHub API Call</button>
-                </div>
                 <div className="btn-group">
                   <button type="button" className="btn btn-primary " onClick={this.invokeGetStatusesForCommit}>Get Commit Statuses</button>
                 </div>
