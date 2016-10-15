02d45871f5b759f4364c954aaa2552fa0d1103ee
compumike08 <compumike08@users.noreply.github.com>
2016-10-15 13:08:47 -0400

    Remove 'Get Commit Statuses' button from HomePage

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,21 +1,9 @@
 import React from 'react';
-import GithubAPI from '../../api/githubAPI';
 import OAuthSignInButton from '../oauth/OAuthSignInButton';
-import {GITHUB_REFS} from '../../utils/constants';
 
 class HomePage extends React.Component {
   constructor(props, context) {
     super(props, context);
-
-    this.invokeGetStatusesForCommit = this.invokeGetStatusesForCommit.bind(this);
-  }
-
-  invokeGetStatusesForCommit(){
-    GithubAPI.getStatusesForCommit(GITHUB_REFS.MASTER).then(statuses => {
-      console.log(statuses);
-    }).catch(errMsg => {
-      console.log(errMsg);
-    });
   }
 
   render() {
@@ -27,16 +15,6 @@ class HomePage extends React.Component {
           <div className="row">
             <div className="col-sm-3">
               <OAuthSignInButton/>
-            </div>
-          </div>
-
-          <div className="row">
-            <div className="col-sm-6">
-              <div className="btn-toolbar">
-                <div className="btn-group">
-                  <button type="button" className="btn btn-primary " onClick={this.invokeGetStatusesForCommit}>Get Commit Statuses</button>
-                </div>
-              </div>
             </div>
           </div>
 
