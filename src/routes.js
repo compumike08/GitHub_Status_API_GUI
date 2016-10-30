af5440d3c887d7fed41a1dc18b0e8eb02465a7a0
compumike08 <compumike08@users.noreply.github.com>
2016-10-30 14:01:10 -0400

    Rename BranchesList class to BranchesPage

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,12 @@ import {Route, IndexRoute} from 'react-router';
 import App from './components/App';
 import HomePage from './components/home/HomePage';
 import OAuthReturner from './components/oauth/OAuthReturner';
-import BranchList from './components/repos/branches/BranchesList';
+import BranchPage from './components/repos/branches/BranchesPage';
 
 export default (
     <Route path="/" component={App}>
         <IndexRoute component={HomePage}/>
         <Route path="oauthreturn" component={OAuthReturner}/>
-        <Route path="repobranches/:id" component={BranchList}/>
+        <Route path="repobranches/:id" component={BranchPage}/>
     </Route>
 );
