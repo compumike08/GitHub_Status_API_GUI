d7745e55e43a88e90d766ecff6751e4b78ebdeb1
compumike08 <compumike08@users.noreply.github.com>
2016-10-30 16:19:39 -0400

    Refactor repoReducer to add empty 'branches' array to each repo object

--- src/reducers/repoReducer.js
+++ src/reducers/repoReducer.js
@@ -4,7 +4,12 @@ import initialState from './initialState';
 export default function repoReducer(state = initialState.repos, action) {
     switch (action.type) {
         case types.REPOS_LOADED:
-            return action.repos;
+            return action.repos.map(repo => {
+              return {
+                ...repo,
+                branches: []
+              }
+            });
         default:
           return state;
   }
