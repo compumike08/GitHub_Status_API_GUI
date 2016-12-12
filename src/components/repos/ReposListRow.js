093da4d603cb163dbf2be6d7dee32f00901d65f1
compumike08 <compumike08@users.noreply.github.com>
2016-12-11 19:10:17 -0500

    Revert "Modify UI so list rows are divs instead of buttons"
    This reverts commit 271bb102d21a6425ed7335d56c8afac41cb6b7e1.

--- src/components/repos/ReposListRow.js
+++ src/components/repos/ReposListRow.js
@@ -2,21 +2,7 @@ import React, {PropTypes} from 'react';
 
 const ReposListRow = ({repo, onSelect}) => {
     return (
-      <div className="list-group-item">
-        <div className="row">
-          <div className="col-lg-1 col-sm-2">
-            <div className="btn-group" role="group">
-              <button type="button" className="btn btn-success" value={repo.id} onClick={onSelect}>
-                View
-              </button>
-            </div>
-          </div>
-
-          <div className="col-lg-9 col-sm-10">
-            <span>{repo.name}</span>
-          </div>
-        </div>
-      </div>
+      <button type="button" className="list-group-item" value={repo.id} onClick={onSelect}>{repo.name}</button>
     );
 };
 
--- src/components/repos/branches/BranchesListRow.js
+++ src/components/repos/branches/BranchesListRow.js
@@ -2,21 +2,7 @@ import React, {PropTypes} from 'react';
 
 const BranchesListRow = ({repoId, branch, onSelect}) => {
   return (
-  <div className="list-group-item">
-    <div className="row">
-      <div className="col-lg-1 col-sm-2">
-        <div className="btn-group" role="group">
-          <button type="button" className="btn btn-success" data-repo-id={repoId} value={branch.name} onClick={onSelect}>
-            View
-          </button>
-        </div>
-      </div>
-
-      <div className="col-lg-9 col-sm-10">
-        <span>{branch.name}</span>
-      </div>
-    </div>
-  </div>
+    <button type="button" className="list-group-item" data-repo-id={repoId} value={branch.name} onClick={onSelect}>{branch.name}</button>
   );
 };
 
