f120a81d79a9267c3ee137a488bc987fa16d7c48
compumike08 <compumike08@users.noreply.github.com>
2017-01-28 17:06:12 -0500

    Add common ErrorNotice React component

---
+++ src/components/common/ErrorNotice.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ErrorNotice = () => {
+  return (
+    <div className="panel-body">
+      <span className="bold italic">An error has occured.</span>
+    </div>
+  );
+};
+
+export default ErrorNotice;
