7d9599a35e37a028dfa6f4adb1075cba793bbeef
compumike08 <compumike08@users.noreply.github.com>
2017-04-17 00:36:26 -0400

    Modify webpack configs to bundle dependencies written to run in node.js

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -46,11 +46,17 @@ export default {
       {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'},
       {test: /\.(jpe?g|png|gif)$/i, loader: 'file?name=[name].[ext]'},
       {test: /\.ico$/, loader: 'file?name=[name].[ext]'},
-      {test: /(\.css|\.scss)$/, loaders: ['style', 'css?sourceMap', 'postcss', 'sass?sourceMap']}
+      {test: /(\.css|\.scss)$/, loaders: ['style', 'css?sourceMap', 'postcss', 'sass?sourceMap']},
+      {test: /\.json$/, loader: 'json-loader'}
     ]
   },
   externals:[{
     xmlhttprequest: '{XMLHttpRequest:XMLHttpRequest}'
   }],
+  node: {
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty'
+  },
   postcss: ()=> [autoprefixer]
 };
